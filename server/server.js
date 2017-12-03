ed42bc51ff8273bde076e9a38ffe8741fe85e203
Sikarin Poonsawat <afumagic3@gmail.com>
2017-12-04 03:13:30 +0700

    http patch update data

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const {ObjectID} = require('mongodb');
+const _ = require('lodash');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -64,6 +65,35 @@ app.delete('/todos/:id', (req , res) => {
     });
 });
 
+app.patch('/todos/:id', (req , res) => {
+    var id = req.params.id ;
+    var body = _.pick(req.body, ['text', 'completed']);
+    
+    if(!ObjectID.isValid(id)){
+        return res.status(404).send();
+    }
+    
+    if(_.isBoolean(body.completed) && body.completed){
+        body.completedAt = new Date().getTime() ;
+    }else{
+        body.completed = false ;
+        body.completedAt = null ;
+    }
+    
+    Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
+        if(!todo){
+            return res.status(404).send();
+        }    
+        res.send({todo});
+    }, (e) => {
+        res.status(404).send();
+    });
+    
+});
+
 app.listen(port, () => {
     console.log('Stared listen on port', port);
 });
+
+
+
