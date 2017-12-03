813943f2b2e6ca2ef63a09f710bd94fcd2c2b86c
Sikarin Poonsawat <afumagic3@gmail.com>
2017-12-03 14:53:25 +0700

    add http remove

---
+++ playground/mongoose-remove.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
+
+var {mongoose} = require('./db/mongoose');
+var {Todo} = require('./models/todo');
+var {User} = require('./models/user');
